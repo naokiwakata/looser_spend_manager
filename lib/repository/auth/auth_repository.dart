088c9ea1f9557926a235467ee3dde4032ce86fb8
naokiwakata <rockwakata15@gmail.com>
2023-07-22 01:27:27 +0900

    コメントの追加

--- lib/repository/auth/auth_repository.dart
+++ lib/repository/auth/auth_repository.dart
@@ -6,10 +6,12 @@ final authRepositoryProvider = Provider.autoDispose((ref) => AuthRepository());
 class AuthRepository {
   final _auth = FirebaseAuth.instance;
 
+  /// 匿名認証を行う
   Future<UserCredential?> signInAnonymously() async {
     return await _auth.signInAnonymously();
   }
 
+  /// Userの認証情報を購読する
   Stream<User?> subscribeUser() {
     return _auth.authStateChanges();
   }
--- lib/repository/firestore/firestore_repository/app_user.dart
+++ lib/repository/firestore/firestore_repository/app_user.dart
@@ -11,6 +11,7 @@ class AppUserRepository {
   Future<void> create({required AppUser appUser}) =>
       appUsersRef.doc(appUser.userId).set(appUser);
 
+  /// [AppUser] を取得する
   Future<AppUser?> fetch({required String userId}) async {
     final ds = await appUserRef(userId).get();
     return ds.data();
