0ad0d4e7ad8a7e012b9af3a6d66e3b1877ce8eb1
naokiwakata <rockwakata15@gmail.com>
2023-07-22 01:08:00 +0900

    remove import

--- lib/repository/firestore/firestore_models/expense/expense.dart
+++ lib/repository/firestore/firestore_models/expense/expense.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutterfire_json_converters/flutterfire_json_converters.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
